2c7f4c4a8a3bca499df72ff57bd0be117acccc80
Ramiro Montero <ramiroamontero@gmail.com>
2025-09-20 14:52:52 -0300

    feat: add phone field to client migration

--- database/migrations/2025_09_19_023820_create_clients_table.php
+++ database/migrations/2025_09_19_023820_create_clients_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
